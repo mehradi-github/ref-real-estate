4aabfa7a6c08201e2f98db7f18c0e975cc29e6fe
Mehradi <mehradi.info@gmail.com>
2023-02-26 07:47:57 +0100

    Adding style items center in navbar

--- src/features/navbar/Navbar.tsx
+++ src/features/navbar/Navbar.tsx
@@ -7,36 +7,39 @@ const Navbar: FC = () => {
       <div className="max-w-6xl mx-auto px-4">
         <div className="flex justify-between">
           <div className="flex space-x-7">
-            <a href="#" className="flex items-center py-4">
-              <HomeIcon className="h-8 w-8" />
-              <span className="font-semibold text-gray-500">Real Estate</span>
-            </a>
-          </div>
-          <div className="hidden md:flex space-x-1">
-            <a
-              className="py-4 px-2 text-gray-500 font-semibold border-green-500 border-b-4"
-              href="#"
-            >
-              Home
-            </a>
-            <a
-              className="py-4 px-2  text-gray-500 font-semibold hover:text-green-500 transition duration-500  "
-              href="#"
-            >
-              Service
-            </a>
-            <a
-              className="py-4 px-2  text-gray-500 font-semibold hover:text-green-500 transition duration-500  "
-              href="#"
-            >
-              About
-            </a>
-            <a
-              className="py-4 px-2  text-gray-500 font-semibold hover:text-green-500 transition duration-500  "
-              href="#"
-            >
-              Contact
-            </a>
+            <div>
+              <a href="#" className="flex items-center py-4">
+                <HomeIcon className="h-8 w-8" />
+                <span className="font-semibold text-gray-500">Real Estate</span>
+              </a>
+            </div>
+
+            <div className="hidden md:flex items-center space-x-1">
+              <a
+                className="py-4 px-2 text-gray-500 font-semibold border-green-500 border-b-4"
+                href="#"
+              >
+                Home
+              </a>
+              <a
+                className="py-4 px-2  text-gray-500 font-semibold hover:text-green-500 transition duration-500  "
+                href="#"
+              >
+                Services
+              </a>
+              <a
+                className="py-4 px-2  text-gray-500 font-semibold hover:text-green-500 transition duration-500  "
+                href="#"
+              >
+                About
+              </a>
+              <a
+                className="py-4 px-2  text-gray-500 font-semibold hover:text-green-500 transition duration-500  "
+                href="#"
+              >
+                Contact Us
+              </a>
+            </div>
           </div>
         </div>
       </div>
